5e603cab6a9b5afea5bf248d9d39114d0fdbb145
VP <51seer.vincent@gmail.com>
2023-07-05 11:32:26 +0800

    Added MLB setup.sh and helm files.
    Signed-off-by: VP <51seer.vincent@gmail.com>

---
+++ LB/1-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# By VP@23.07.05
+# ref: https://metallb.universe.tf/installation/
+
+read -p "我假設你已經裝好 Kubernetes ，沒問題的話按 Enter 繼續" Enter
+
+echo "讓你看看有什麼會被改"
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+sleep 3
+
+echo "幫你搞好了"
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+echo "Apply MetalLB YAML"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
